45a9b936d6479310139b0fe4c9976d070e19233e
mashukai <2978730840@qq.com>
2020-10-09 13:41:04 +0800

    10.9

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <table border="1"> 
     <tr>
         <td>id</td>
@@ -14,7 +22,23 @@
         <td>{{$v->email}}</td>
         <td>{{ date( "Y-m-d H : i : s", $v->time)}}</td>
         <td>{{$v->ip}}</td>
-        <td><a href="{{}}">删除</a><a href="">修改</a></td>
+        <td><a class="li" id="{{$v->uid}}">删除</a><a href="">修改</a></td>
     </tr>
     @endforeach
 </table>
+</body>
+</html>
+<script src="/static/js/jquery.min.js"></script>
+<script>
+    $('.li').click(function(res){
+        // alert(111);exit;
+        var id = $(this).attr('id');
+        if(confirm('您确定要删除吗')){
+        $.get('/admin/destory/'+id,function(res){
+            if(res.code=='00000'){
+                location.href="/admin";
+            }
+        },'json');
+        }
+    });
+</script>
